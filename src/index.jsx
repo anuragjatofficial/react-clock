543dfdc470fdc125787586ca944456937f580a3b
anuragjatofficial <anurag.1101140@gmail.com>
2023-09-13 23:02:48 +0530

    updated timer logic

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,50 @@
 function App() {
   return (
-    <div>
-        <Clock/>
+    <div className="w-screen h-screen">
+      <Clock />
     </div>
-  )
+  );
 }
 
 function Clock(){
 
-    const [breakLength,setBreakLength] = React.useState(0);
-    const [sessionLength, setSessionLength] = React.useState(0);
+    const [breakLength,setBreakLength] = React.useState(5);
+    const [sessionLength, setSessionLength] = React.useState(25);
+    const [play, setPlay] = React.useState(false);
+
+
+    const handlePlayPause = ()=>{
+        setPlay(!play);
+    };
+
+    React.useEffect(() => {
+      console.log(play);
+      let interval;
+      if (play) {
+        interval = setInterval(() => {
+          setSessionLength((prevSessionLength) => prevSessionLength - 1);
+        }, 1000);
+      }
+
+      if (setSessionLength === 0) {
+        setPlay(false);
+        clearInterval(interval); // Stop the timer when it reaches 0
+      }
+
+      return () => {
+        clearInterval(interval); // Clean up the interval on unmount or when isActive changes
+      };
+    }, [play, setPlay, sessionLength, sessionLength]);
+
+    const handleRefresh = ()=>{
+        setSessionLength(25);
+        setBreakLength(5);
+    };
 
     return (
-      <div className="w-screen h-screen">
-        <div className="flex justify-around">
-          <div className="flex flex-col">
+      <div className="flex items-center flex-col">
+        <div className="flex-row flex">
+          <div>
             <div id="break-label">Break Length</div>
             <div className="flex">
               <button
@@ -78,14 +108,30 @@ function Clock(){
         </div>
 
         {/* timer */}
+
         <div>
-          <div id="timer-label"></div>
-          <div id="time-left"></div>
+          <div id="timer-label">Session</div>
+          <div id="time-left">{sessionLength+`:00`}</div>
         </div>
+
         {/* clickables */}
+
         <div>
-          <button className="btn-primary" id="start_stop"></button>
-          <button className="btn-primary" id="reset"></button>
+          <button
+            className="btn-primary"
+            id="start_stop"
+            onClick={handlePlayPause}
+          >
+            {play ? (
+              <i className="fa fa-pause" aria-hidden="true"></i>
+            ) : (
+              <i className="fa fa-play" aria-hidden="true"></i>
+            )}
+          </button>
+          <button className="btn-primary" id="reset"
+          onClick={handleRefresh}>
+            <i className="fa fa-refresh" aria-hidden="true"></i>
+          </button>
         </div>
       </div>
     );
